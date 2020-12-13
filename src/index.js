57728e11651bd123bec0287202e18a9786122063
Veras <sabrina.alves@drcash.com.br>
2020-12-13 18:41:29 -0300

    add routes to index

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// routes
+require("./routes/bookRoute")(app)
+
 app.listen(process.env.PORT, () => {
     console.log("running")
 })
