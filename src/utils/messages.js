ff4fb0b1849816b7186f60123f043149bd5c1205
Veras <sabrina.alves@drcash.com.br>
2020-12-13 20:19:39 -0300

    add messages file

---
+++ src/utils/messages.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    book:{
+        bookCreate: "Book create successful",
+        bookNotFound: "There aren't books registered in the database",
+        bookNotFoundID: "There isn't book registered with this id in the database"
+    }
+}
