da2de9134cfa2f7cbf490eb71edbea5cafaadfdd
Veras <sabrina.alves@drcash.com.br>
2020-12-13 20:28:06 -0300

    update the messages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,8 +1,13 @@
 module.exports = {
 
     book:{
-        bookCreate: "Book create successful",
+        bookCreated: "Book create successful",
+        bookAll: "These are all books registered in the database",
+        bookID: "These is the book registered with this id in the database",
+        bookUpdated: "The book registered with this id was updated in the database",
+        bookDeleted: "The book registered with this id was deleted in the database",
         bookNotFound: "There aren't books registered in the database",
-        bookNotFoundID: "There isn't book registered with this id in the database"
+        bookNotFoundID: "There isn't book registered with this id in the database",
+        bookAlreadyExist: "This book already exists in the database"
     }
 }
